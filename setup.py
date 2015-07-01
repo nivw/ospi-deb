71f66f6fd1ccd508d949990fe3a02ab383880b21
Niv Gal Waizer <nivwiz@gmail.com>
2015-07-02 00:27:10 +0300

    we must have__init__ for findpackags()

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ _in_same_dir = functools.partial( os.path.join, os.path.dirname( __file__ ) )
 
 data_dir=( 'templates', 'static', 'i18n', 'data' )
 data_files=[]
-
 for directory in data_dir:
   for root, dirs, files in os.walk( 'ospi/'+directory ):
     for f in files:
@@ -18,12 +17,16 @@ for directory in data_dir:
     if len(files) and '/.' not in root:
       data_files.append( ( root, [ os.path.join( root, f) for f in files] ))
 
-with open('data_files','w') as f:
+with open('data_files.json','w') as f:
   json.dump(data_files, f)
-  
+
+with open('packages.json', 'w') as f:
+  json.dump( find_packages(), f )
+      
 with open(_in_same_dir("__version__.py")) as version_file:
   exec(version_file.read()) # pylint: disable=W0122
 
+  
 setup(name='ospi',
       classifiers=[
                     "Programming Language :: Python :: 2.7",
@@ -43,19 +46,3 @@ setup(name='ospi',
                       ]
               },
       )
-
-"""
-                         ('data', glob.glob('ospi/data/*') ),
-                         ('blinker', glob.glob('ospi/blinker/*') ),
-                         ('static/plugins', glob.glob('ospi/static/plugins/*') ),
-                         ('plugins/manifests', glob.glob('ospi/plugins/manifests/*') ),
-                         ('i18n/cs_CZ', glob.glob('ospi/i18n/cs_CZ/*') ),
-                         ('i18n/fr_FR', glob.glob('ospi/i18n/fr_FR/*') ),
-                         ('i18n/es_ES', glob.glob('ospi/i18n/es_ES/*') ),
-                         ('i18n/en_US', glob.glob('ospi/i18n/en_US/*') ),
-                         ('i18n/de_DE', glob.glob('ospi/i18n/de_DE/*') ),
-                         ('i18n/sl_SL', glob.glob('ospi/i18n/sl_SL/*') ),
-                         ('i18n', glob.glob('ospi/i18n/*') ),
-                         ('templates', glob.glob('ospi/templates/*') ),
-                         ('web', glob.glob('ospi/web/*') ),
-"""
