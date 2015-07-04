1f124bb6f5b41be7f09162de595aa7174afa1ead
Niv Gal Waizer <nivwiz@gmail.com>
2015-07-05 01:23:07 +0300

    Install non py files at the same dir and fix service

--- setup.py
+++ setup.py
@@ -7,26 +7,34 @@ import json
 _PYTHON_VERSION = platform.python_version()
 _in_same_dir = functools.partial( os.path.join, os.path.dirname( __file__ ) )
 
+project_name='OSPi'
 data_dir=( 'templates', 'static', 'i18n', 'data' )
-data_files=[]
+package_data=[]
 for directory in data_dir:
-  for root, dirs, files in os.walk( 'ospi/'+directory ):
-    for f in files:
-      if f.startswith('.') :
-        files.remove(f)
-    if len(files) and '/.' not in root:
-      data_files.append( ( root, [ os.path.join( root, f) for f in files] ))
+  for root, dirs, files in os.walk( os.path.join(project_name, directory) ):
+    if len(files) :
+      temp_list=root.split('/')
+      temp_list.remove(project_name)
+      path='/'.join( temp_list )
+      package_data.extend( [ os.path.join( path, f) for f in files] )
 
 with open('data_files.json','w') as f:
   json.dump(data_files, f)
 
 with open('packages.json', 'w') as f:
   json.dump( find_packages(), f )
+
+with open('package_data', 'w') as f:
+  for s in package_data:
+        f.write(s + '\n')
       
 with open(_in_same_dir("__version__.py")) as version_file:
   exec(version_file.read()) # pylint: disable=W0122
 
-  
+install_requires = [
+  "Adafruit_BBIO",
+  ]
+
 setup(name='ospi',
       classifiers=[
                     "Programming Language :: Python :: 2.7",
@@ -39,7 +47,8 @@ setup(name='ospi',
       version=__version__, # pylint: disable=E0602
       url='https://github.com/Dan-in-CA/OSPi',
       packages=find_packages(exclude=["tests"]),
-      data_files=data_files,
+      include_package_data=True,
+      install_requires=install_requires,
       entry_points={
               'console_scripts': [
                       'ospi = ospi:run',
